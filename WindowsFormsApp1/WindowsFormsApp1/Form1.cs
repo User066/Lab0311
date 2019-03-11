5517af1d263832416c95561bd9466e6c2a41beba
chan chan <a0958500055@outlook.com>
2019-03-11 13:53:41 +0800

    edit btn1

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Text = "HELLO";
+            //edit btn1
         }
     }
 }
